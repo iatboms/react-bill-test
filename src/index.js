f78eb394f3b9b845dfa3f69e50a8b1a19eed1b14
LAPTOP-4KPO1DFB\wangx <wangxiaojjyy@gmail.com>
2023-12-25 18:33:51 +0900

    主题化定制

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 import App from "./App"
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
-
+// 导入定制主题
+import '@/theme.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/page/Layout/index.js
+++ src/page/Layout/index.js
@@ -1,8 +1,14 @@
+import { Button } from "antd-mobile"
 import { Outlet } from "react-router-dom"
 
-const Layout = ()=>{
+const Layout = () => {
     return <div>this is Layout.
         <Outlet></Outlet>
+        {/* 测试全局生效样式 */}
+        <Button color="primary">测试全局</Button>
+        <div className="purple-theme">
+            <Button color="primary">测试局部</Button>
         </div>
+    </div>
 }
 export default Layout
