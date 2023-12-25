99d86f4752f9759f70a98e438454f6a4e4b7bb5e
LAPTOP-4KPO1DFB\wangx <wangxiaojjyy@gmail.com>
2023-12-25 19:49:04 +0900

    TabBar实现

--- src/page/Layout/index.js
+++ src/page/Layout/index.js
@@ -1,24 +1,58 @@
 import { getBillList } from "@/store/modules/billStore"
-import { Button } from "antd-mobile"
+import { Button, TabBar } from "antd-mobile"
 import { useEffect } from "react"
 import { useDispatch } from "react-redux"
-import { Outlet } from "react-router-dom"
+import { Outlet, useNavigate } from "react-router-dom"
+import './index.scss'
+import {
+    BillOutline,
+    CalculatorOutline,
+    AddCircleOutline
+} from 'antd-mobile-icons'
+
+const tabs = [
+    {
+        key: '/month',
+        title: '月度账单',
+        icon: <BillOutline></BillOutline>
+    },
+    {
+        key: '/new',
+        title: '记账',
+        icon: <AddCircleOutline></AddCircleOutline>
+    },
+    {
+        key: '/year',
+        title: "年度账单",
+        icon: <CalculatorOutline></CalculatorOutline>
+    }
+]
+
 
 const Layout = () => {
     const dispatch = useDispatch()
-    useEffect(()=>{
+    useEffect(() => {
         dispatch(getBillList())
-    },[dispatch])
+    }, [dispatch])
+const navigate = useNavigate()
+const switchRoute = (path)=>{
+    console.log(path)
+    navigate(path)
+}
 
-
-
-    return <div>this is Layout.
-        <Outlet></Outlet>
-        {/* 测试全局生效样式 */}
-        <Button color="primary">测试全局</Button>
-        <div className="purple-theme">
-            <Button color="primary">测试局部</Button>
+    return (
+        <div className="layout">
+            <div className="container">
+                <Outlet></Outlet>
+            </div>
+            <div className="footer">
+                <TabBar onChange={switchRoute}>
+                    {tabs.map(item => (
+                        <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+                    ))}
+                </TabBar>
+            </div>
         </div>
-    </div>
+    )
 }
 export default Layout
